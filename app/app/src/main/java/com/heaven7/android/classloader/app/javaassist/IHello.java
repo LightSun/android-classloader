3fe0d068fe88fd3096b867f65ef2019c9f5ba987
LightSun <donshine723@gmail.com>
2020-06-28 15:59:05 +0800

    opt

--- app/app/src/main/java/com/heaven7/android/classloader/app/javaassist/IHello.java
+++
@@ -1,6 +0,0 @@
-package com.heaven7.android.classloader.app.javaassist;
-
-public interface IHello {
-
-    String hello(String in);
-}
--- app/app/src/main/java/com/heaven7/android/classloader/app/javaassist/TestUtil.java
+++
@@ -1,8 +0,0 @@
-package com.heaven7.android.classloader.app.javaassist;
-
-public final class TestUtil {
-
-    public static String hello(String in){
-        return "hello_world_" + in;
-    }
-}
